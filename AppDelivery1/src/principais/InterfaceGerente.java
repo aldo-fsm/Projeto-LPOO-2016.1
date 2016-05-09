881edb68910a1c0e349695ea5e0c267ebd90bb18
Aryell <Aryell@pc-user>
2016-05-09 20:29:56 -0300

    ***interface gerente, ops kkkkkkkkk

--- AppDelivery1/src/principais/InterfaceGerente.java
+++ AppDelivery1/src/principais/InterfaceGerente.java
@@ -14,11 +14,12 @@ public class InterfaceGerente {
 		Scanner sc = new Scanner(System.in);
 		Gerente novo = DataBase.lerBaseGerente();
 		do {
-			System.out.println("1. Adicionar/Cadastar um cliente\n2. Remover um Cliente");
+			System.out.println(
+					"1. Adicionar/Cadastar um cliente\n2. Remover um Cliente\n3. adicionar restaurante\n4. remover restaurante\n5.adicionar prato\n6.remover prato");
 			int opcao = sc.nextInt();
 			if (opcao == 1) {
-				//  Adicionar/Cadastar um cliente:
-				//  Pede as informações inerentes ao Cliente;
+				// Adicionar/Cadastar um cliente:
+				// Pede as informações inerentes ao Cliente;
 				System.out.println("nome do cliente: ");
 				sc.nextLine();
 				String nomeDoCliente = sc.nextLine();
@@ -26,27 +27,27 @@ public class InterfaceGerente {
 				String loginDoCliente = sc.nextLine();
 				System.out.println("senha do cliente: ");
 				String senhaDoCliente = sc.nextLine();
-				//  Instacia o Cliente;
+				// Instacia o Cliente;
 				Cliente Aryell = new Cliente(loginDoCliente, senhaDoCliente, nomeDoCliente);
-				//  Adiciona o novo Cliente ao sistema;
+				// Adiciona o novo Cliente ao sistema;
 				novo.adicionarCliente(Aryell);
-				//  Salva o estado do sistema;
+				// Salva o estado do sistema;
 				DataBase.salvarEstado(novo.repositorioC());
 			} else if (opcao == 2) {
-				//  Remover um Cliente:
-				//  Uma lista com todos os clientes cadastrados e seus
+				// Remover um Cliente:
+				// Uma lista com todos os clientes cadastrados e seus
 				// respectivos ID’s deve ser exibida;
 				novo.listarClientes();
-				//  Solicitar o ID do Cliente que deve ser removido;
+				// Solicitar o ID do Cliente que deve ser removido;
 				System.out.println("informe o id do cliente a ser removido");
 				int id = sc.nextInt();
-				//  Remover o Cliente cujo ID foi fornecido.
+				// Remover o Cliente cujo ID foi fornecido.
 				novo.removerCliente(id);
-				//  Salva o estado do sistema;
+				// Salva o estado do sistema;
 				DataBase.salvarEstado(novo.repositorioC());
 			} else if (opcao == 3) {
-				//  Adicionar um Restaurante
-				//  Pede as informações inerentes ao Restaurante;
+				// Adicionar um Restaurante
+				// Pede as informações inerentes ao Restaurante;
 				System.out.println("nome do restaurante : ");
 				sc.nextLine();
 				String nomeDoRestaurante = sc.nextLine();
@@ -54,32 +55,33 @@ public class InterfaceGerente {
 				String loginDoRestaurante = sc.nextLine();
 				System.out.println("senha do restaurante: ");
 				String senhaDoRestaurante = sc.nextLine();
-				//  Instacia o Restaurante;
+				// Instacia o Restaurante;
 				Restaurante Atlantico = new Restaurante(loginDoRestaurante, senhaDoRestaurante, nomeDoRestaurante);
-				//  Adiciona o novo Restaurante ao sistema;
+				// Adiciona o novo Restaurante ao sistema;
 				novo.adicionarRestaurante(Atlantico);
-				//  Salva o estado do sistema
+				// Salva o estado do sistema
 				DataBase.salvarEstado(novo.repositorioR());
 			} else if (opcao == 4) {
-				//  Remover um Restaurante
-				//  Uma lista com todos os Restaurantes cadastrados e seus
+				// Remover um Restaurante
+				// Uma lista com todos os Restaurantes cadastrados e seus
 				// respectivos ID’s deve ser exibida;
 				novo.listarRestaurantes();
-				//  Solicitar o ID do Restaurante que deve ser removido;
+				// Solicitar o ID do Restaurante que deve ser removido;
 				System.out.println("informe o id do restaurante a ser removido");
 				int idDoRestaurnate = sc.nextInt();
-				//  Remover o Restaurante cujo ID foi fornecido.
+				// Remover o Restaurante cujo ID foi fornecido.
 				novo.removerRestaurante(idDoRestaurnate);
-				//  Salva o estado do sistema
+				// Salva o estado do sistema
 				DataBase.salvarEstado(novo.repositorioR());
 			} else if (opcao == 5) {
-				//  Adicionar Prato
-				//  Exibe uma lista com todos os Restaurantes cadastrados e seus respectivos ID’s;
+				// Adicionar Prato
+				// Exibe uma lista com todos os Restaurantes cadastrados e seus
+				// respectivos ID’s;
 				novo.listarRestaurantes();
-				//  Solicitar o ID do Restaurante que deve receber o novo prato;
+				// Solicitar o ID do Restaurante que deve receber o novo prato;
 				System.out.println("informe o id do restaurante a receber o novo prato");
 				int idDoRestaurante = sc.nextInt();
-				//  Solicita as informações inerentes ao novo prato;
+				// Solicita as informações inerentes ao novo prato;
 				System.out.println("nome do prato : ");
 				sc.nextLine();
 				String nomeDoPrato = sc.nextLine();
@@ -87,26 +89,28 @@ public class InterfaceGerente {
 				long idDoPrato = sc.nextLong();
 				System.out.println("informe o preco do prato: ");
 				double precoDoPrato = sc.nextDouble();
-				// Instancia e adiciona o novo prato 
+				// Instancia e adiciona o novo prato
 				ItemCardapio novoPrato = new ItemCardapio(idDoPrato, nomeDoPrato, precoDoPrato);
 				novo.adicionarPrato(idDoRestaurante, novoPrato);
 				// Salva o estado do sistema;
 				DataBase.salvarEstado(novo.repositorioR());
-			}else if(opcao == 6){
-				//  Remover Prato 
-				//  Exibe uma lista com todos os Restaurantes cadastrados e  seus respectivos ID’s;
+			} else if (opcao == 6) {
+				// Remover Prato
+				// Exibe uma lista com todos os Restaurantes cadastrados e seus
+				// respectivos ID’s;
 				novo.listarRestaurantes();
-                //  Solicitar o ID do Restaurante que deve ter o prato removido de seu Cardápio; 
+				// Solicitar o ID do Restaurante que deve ter o prato removido
+				// de seu Cardápio;
 				System.out.println("informe o id do restaurante a remover o prato");
 				int idDoRestaurante = sc.nextInt();
-				//  Exibe o Cardápio desse Restaurante e os ID’s de cada item; 
+				// Exibe o Cardápio desse Restaurante e os ID’s de cada item;
 				novo.listarCardapio(idDoRestaurante);
-				//  Solicitar o ID do prato a ser Removido; 
+				// Solicitar o ID do prato a ser Removido;
 				System.out.println("informe o id do prato a ser removido");
-				int idDoPrato= sc.nextInt();
-				//  Remover o prato; 
+				int idDoPrato = sc.nextInt();
+				// Remover o prato;
 				novo.removerPrato(idDoRestaurante, idDoPrato);
-				//  Salva o estado do sistema;   
+				// Salva o estado do sistema;
 				DataBase.salvarEstado(novo.repositorioR());
 			}
 
