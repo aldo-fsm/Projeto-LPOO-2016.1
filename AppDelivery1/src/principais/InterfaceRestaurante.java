b63d28880f92edbbac75b4a3864149793a70a8d3
Aldo <Aldo@192.168.1.6>
2016-05-09 18:20:07 -0300

    Tela do restaurante logado

--- AppDelivery1/src/principais/InterfaceRestaurante.java
+++ AppDelivery1/src/principais/InterfaceRestaurante.java
@@ -13,9 +13,11 @@ import javax.swing.JTextField;
 
 
 public class InterfaceRestaurante extends JFrame implements ActionListener {
-	private JButton loginButton;
-	//private JButton cadastroButton;
 	private JButton loginOkButton;
+	private JButton cadastrarItemButton;
+	private JButton removerItemButton;
+	private JButton listarPedidosButton;
+	private JButton logoutButton;
 	private JTextField campoTextoLogin;
 	private JPasswordField campoSenhaLogin;
 	private CardLayout cL = new CardLayout();
@@ -28,29 +30,18 @@ public class InterfaceRestaurante extends JFrame implements ActionListener {
 		setSize(800, 600);
 		setResizable(false);
 		
-		//JPanel telaInicial = new JPanel(null);
-		JPanel telaLogin = new JPanel(null);
-		//JPanel telaCadastro = new JPanel(null);
+		JPanel telaInicial = new JPanel(null);
+		JPanel telaLogado = new JPanel(null);
 		
-		//cards.add(telaInicial,"tela inicial");
-		cards.add(telaLogin,"tela de login");
-		//cards.add(telaCadastro,"tela de cadastro");
+		cards.add(telaLogado, "logado");
+		cards.add(telaInicial,"tela inicial");
 		
 		add(cards);
-	
-		
-/*		loginButton = new JButton("Login");
-		loginButton.addActionListener(this);
-		cadastroButton = new JButton("Cadastro");
-		loginButton.setBounds(300, 300, 200, 70);
-		cadastroButton.setBounds(300, 400, 200, 70);
-		telaInicial.add(loginButton);
-		telaInicial.add(cadastroButton);*/
-		
+			
 		campoTextoLogin = new JTextField();
 		campoSenhaLogin = new JPasswordField();
-		//cadastroButton = new JButton("Cadastro");
 		loginOkButton = new JButton("OK");
+		loginOkButton.addActionListener(this);
 		JLabel loginNome = new JLabel("Login :");
 		JLabel loginSenha = new JLabel("Senha :");
 		campoTextoLogin.setBounds(300, 350, 200, 30);
@@ -58,16 +49,27 @@ public class InterfaceRestaurante extends JFrame implements ActionListener {
 		loginNome.setBounds(250, 350, 50, 30);
 		loginSenha.setBounds(250, 400, 50, 30);
 		loginOkButton.setBounds(350, 450, 100, 30);
-		telaLogin.add(campoTextoLogin);
-		telaLogin.add(campoSenhaLogin);
-		telaLogin.add(loginNome);
-		telaLogin.add(loginSenha);
-		telaLogin.add(loginOkButton);
+		telaInicial.add(campoTextoLogin);
+		telaInicial.add(campoSenhaLogin);
+		telaInicial.add(loginNome);
+		telaInicial.add(loginSenha);
+		telaInicial.add(loginOkButton);
 		
+		cadastrarItemButton = new JButton("Adicionar Item no Cardapio");
+		removerItemButton = new JButton("Remover Item do Cardapio");
+		listarPedidosButton = new JButton("Listar Pedidos em Espera");
+		logoutButton = new JButton("Logout");
+		logoutButton.addActionListener(this);
+		cadastrarItemButton.setBounds(290, 280, 220, 50);
+		removerItemButton.setBounds(290, 350, 220, 50);
+		listarPedidosButton.setBounds(290, 420, 220, 50);
+		logoutButton.setBounds(300, 500, 200, 30);
+		telaLogado.add(cadastrarItemButton);
+		telaLogado.add(removerItemButton);
+		telaLogado.add(listarPedidosButton);
+		telaLogado.add(logoutButton);
 		
-		
-		
-		cL.show(cards, "tela de login");
+		cL.show(cards, "tela inicial");
 	}
 
 	public static void main(String[] args) {
@@ -77,13 +79,15 @@ public class InterfaceRestaurante extends JFrame implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		
-		if(e.getSource().equals(loginButton)){
 			
-			cL.show(cards, "tela de login");
-			
-		}
 		if(e.getSource().equals(loginOkButton)){
 			
+			
+			
+			cL.show(cards, "logado");
+		}
+		if(e.getSource().equals(logoutButton)){
+			cL.show(cards, "tela inicial");
 		}
 		
 		
