2b566021381d162081c9b05bb1d17862d27dffa8
Aldo <Aldo@192.168.1.5>
2016-05-14 17:22:27 -0300

    interface grafica gerente adicionar prato

--- AppDelivery1/src/principais/InterfaceGraficaGerente.java
+++ AppDelivery1/src/principais/InterfaceGraficaGerente.java
@@ -20,6 +20,7 @@ import javax.swing.JTextField;
 import dados.DataBase;
 import entidades.Cliente;
 import entidades.Gerente;
+import entidades.ItemCardapio;
 import entidades.Restaurante;
 
 public class InterfaceGraficaGerente extends JFrame implements ActionListener {
@@ -30,7 +31,7 @@ public class InterfaceGraficaGerente extends JFrame implements ActionListener {
 	private JButton removerRestauranteButton;
 	private JButton adicionarPratoButton;
 	private JButton removerPratoButton;
-	
+
 	private Gerente gerente;
 
 	public InterfaceGraficaGerente() {
@@ -49,16 +50,16 @@ public class InterfaceGraficaGerente extends JFrame implements ActionListener {
 		removerClienteButton = new JButton("Remover Cliente");
 		cadastrarRestauranteButton = new JButton("Cadastrar Restaurante");
 		removerRestauranteButton = new JButton("remover Restauramte");
-		adicionarPratoButton = new JButton("Adicionar Prato"); 
+		adicionarPratoButton = new JButton("Adicionar Prato");
 		removerPratoButton = new JButton("Remover Prato");
-		
+
 		cadastrarClienteButton.addActionListener(this);
 		removerClienteButton.addActionListener(this);
 		cadastrarRestauranteButton.addActionListener(this);
 		removerRestauranteButton.addActionListener(this);
 		adicionarPratoButton.addActionListener(this);
 		removerPratoButton.addActionListener(this);
-		
+
 		cadastrarClienteButton.setBounds(290, 250, 220, 30);
 		removerClienteButton.setBounds(290, 290, 220, 30);
 		cadastrarRestauranteButton.setBounds(290, 340, 220, 30);
@@ -152,10 +153,11 @@ public class InterfaceGraficaGerente extends JFrame implements ActionListener {
 		atualizarDataBase();
 		return true;
 	}
-	public boolean adicionarPrato(){
+
+	public boolean adicionarPrato() {
 		JPanel painel = new JPanel(null);
 		painel.setPreferredSize(new Dimension(500, 300));
-		
+
 		JLabel label1 = new JLabel("Escolha um restaurante");
 		JLabel label2 = new JLabel("Dados do Prato :");
 		JLabel label3 = new JLabel("Nome");
@@ -165,17 +167,16 @@ public class InterfaceGraficaGerente extends JFrame implements ActionListener {
 		JTextField campoTextoPreco = new JTextField();
 
 		cb.setBackground(Color.WHITE);
-		campoTextoNome.setBounds(50,150,300,20);
-		campoTextoPreco.setBounds(50,180,300,20);
-		cb.setBounds(50,40,300,30);
-		label1.setBounds(50,10,300,20);
-		label2.setBounds(50,90,300,20);
-		label3.setBounds(10,150,300,20);
-		label4.setBounds(10,180,300,20);
-		Restaurante restaurante;
-		for(int i = 0;i<gerente.repositorioR().getNumeroRestaurantes();i++){
-			restaurante = gerente.repositorioR().getRestaurante(i);
-			cb.addItem("Nome : "+restaurante.getNome()+", "+"id : "+restaurante.getId());
+		campoTextoNome.setBounds(50, 150, 300, 20);
+		campoTextoPreco.setBounds(50, 180, 300, 20);
+		cb.setBounds(50, 40, 300, 30);
+		label1.setBounds(50, 10, 300, 20);
+		label2.setBounds(50, 90, 300, 20);
+		label3.setBounds(10, 150, 300, 20);
+		label4.setBounds(10, 180, 300, 20);
+		for (int i = 0; i < gerente.repositorioR().getNumeroRestaurantes(); i++) {
+			Restaurante restaurante = gerente.repositorioR().getRestaurante(i);
+			cb.addItem("Nome : " + restaurante.getNome() + ", " + "id : " + restaurante.getId());
 		}
 		painel.add(cb);
 		painel.add(label1);
@@ -184,11 +185,29 @@ public class InterfaceGraficaGerente extends JFrame implements ActionListener {
 		painel.add(label4);
 		painel.add(campoTextoNome);
 		painel.add(campoTextoPreco);
-		int n = JOptionPane.showOptionDialog(null, painel, "Adicionar prato", JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE,null,null,null);
-		if(n==0){
+		int n = JOptionPane.showOptionDialog(null, painel, "Adicionar prato", JOptionPane.OK_CANCEL_OPTION,
+				JOptionPane.QUESTION_MESSAGE, null, null, null);
+		if (n == 0) {
+			try{
+			String nome = campoTextoNome.getText();
+			Double preco = Double.parseDouble(campoTextoPreco.getText());
+
+			if (nome == null || nome.equals("")) {
+				return false;
+			} else if (nome.contains(";") || nome.contains("/")) {
+				JOptionPane.showMessageDialog(this, " \" ; \" e \" / \" não são caracteres válidos");
+				return false;
+			}
+			gerente.adicionarPrato(cb.getSelectedIndex(), new ItemCardapio(nome, preco));
+			atualizarDataBase();
+			return true;
+			}catch(NumberFormatException e){
+				return false;
+			}
 			
+		}else{
+			return false;
 		}
-		return true;
 	}
 
 	public static void main(String[] args) {
@@ -210,12 +229,12 @@ public class InterfaceGraficaGerente extends JFrame implements ActionListener {
 						JOptionPane.ERROR_MESSAGE);
 			}
 		}
-		if(e.getSource().equals(adicionarPratoButton)){
-			if(!adicionarPrato()){
-				
+		if (e.getSource().equals(adicionarPratoButton)) {
+			if (!adicionarPrato()) {
+				JOptionPane.showMessageDialog(this, "Não foi possivel cadastrar o prato", "Erro",
+						JOptionPane.ERROR_MESSAGE);
 			}
 		}
-		
 
 	}
 
