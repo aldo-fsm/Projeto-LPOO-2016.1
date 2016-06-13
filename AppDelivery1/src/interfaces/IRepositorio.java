a003acbebb3f9f444b75cf52857cee8d2549db15
Aldo <Aldo@notebook-Aldo>
2016-06-13 13:59:02 -0300

    repositorio metodos adicionar, remover e alterar

--- AppDelivery1/src/interfaces/IRepositorio.java
+++ AppDelivery1/src/interfaces/IRepositorio.java
@@ -5,18 +5,20 @@ import excecoes.RepositorioCheioException;
 import excecoes.SenhaInvalidaException;
 
 public interface IRepositorio <T> {
+	
+	public static final int MAX_NUMERO_ELEMENTOS = 100;
 
 	public void adicionar(T elemento) throws IdInvalidoException, SenhaInvalidaException, RepositorioCheioException;
 
 	public void remover(int id);
 
-	public void alterar(int id);
+	public void alterar(int id, T novoElemento) throws IdInvalidoException, SenhaInvalidaException;
 	
-	public T getElemento(int id);
+	public T get(int id);
 
 	public T[] copiar();
 
-	public T getCopia(long id);
+	public T getCopia(int id);
 
 	public int getNumeroElementos();
 
--- AppDelivery1/src/repositorios/Repositorio.java
+++ AppDelivery1/src/repositorios/Repositorio.java
@@ -1,5 +1,98 @@
 package repositorios;
 
-public class Repositorio <T> {
+import java.util.ArrayList;
+
+import entidades.Usuario;
+import excecoes.IdInvalidoException;
+import excecoes.RepositorioCheioException;
+import excecoes.SenhaInvalidaException;
+import interfaces.IRepositorio;
+
+public class Repositorio<T> implements IRepositorio<T> {
+
+	private ArrayList<T> elementos = new ArrayList<T>();
+	private int proximoId = 1;
+
+	@Override
+	public void adicionar(T elemento) throws IdInvalidoException, SenhaInvalidaException, RepositorioCheioException {
+		if (elementos.size() < IRepositorio.MAX_NUMERO_ELEMENTOS) {
+			if (elemento instanceof Usuario) {
+				((Usuario) elemento).validar();
+				elementos.add(elemento);
+			} else {
+				elementos.add(elemento);
+			}
+			proximoId++;
+		} else {
+			throw new RepositorioCheioException();
+		}
+
+	}
+
+	@Override
+	public void remover(int id) {
+		if (id >= 0 && id < elementos.size()) {
+			elementos.remove(id);
+		}
+	}
+
+	@Override
+	public void alterar(int id, T novoElemento) throws IdInvalidoException, SenhaInvalidaException {
+		if (id >= 0 && id < elementos.size()) {
+			if (novoElemento instanceof Usuario) {
+				((Usuario) novoElemento).validar();
+				elementos.set(id, novoElemento);
+			} else {
+				elementos.set(id, novoElemento);
+			}
+		}
+	}
+
+	@Override
+	public T get(int id) {
+		return elementos.get(id);
+	}
+
+	@Override
+	public T[] copiar() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T getCopia(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getNumeroElementos() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setProximoId(long proximoId) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public long getProximoId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setNumeroElementos(int numeroElementos) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setElementos(T[] elementos) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
